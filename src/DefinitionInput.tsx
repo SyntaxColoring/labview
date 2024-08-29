bf674ce280d8db6469909878501d6a4e69edf1e8
Max Marrone <max@marrone.nyc>
2024-08-28 23:51:53 -0400

    Install Prettier.

--- src/DefinitionInput.tsx
+++ src/DefinitionInput.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import classes from "./DefinitionInput.module.css";
 
 interface Props {
-	content: string;
-	onChange: (newContent: string) => void;
-	error: string | null;
+  content: string;
+  onChange: (newContent: string) => void;
+  error: string | null;
 }
 
 export default function DefinitionInput(props: Props): React.JSX.Element {
-	return (
-		<div className={classes.container}>
-			<textarea
-				className={classes.input}
-				placeholder="Paste a labware definition here."
-				onChange={(e) => props.onChange(e.target.value)}
-			>
-				{props.content}
-			</textarea>
-			{props.error && <div className={classes.error}>{props.error}</div>}
-		</div>
-	);
+  return (
+    <div className={classes.container}>
+      <textarea
+        className={classes.input}
+        placeholder="Paste a labware definition here."
+        onChange={(e) => props.onChange(e.target.value)}
+      >
+        {props.content}
+      </textarea>
+      {props.error && <div className={classes.error}>{props.error}</div>}
+    </div>
+  );
 }
